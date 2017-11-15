6d64fe2f60d14a76ddc06814ec28ea064bac9696
Nirvan <nshewraj16@gmail.com>
2017-11-15 22:28:41 +0400

    Loop through database and added code to create "users.xml"

--- process_userDbDataToXml.php
+++ process_userDbDataToXml.php
@@ -9,3 +9,30 @@
 
     $userQuery = "SELECT * FROM register";
     $userQueryResult = mysql_query($userQuery) or die ('Query Failed' . mysql_error());
+
+    $userXmlData = "<?xml version='1.0' encoding='UTF-8'?>
+    <users>";
+
+    while ($row = mysql_fetch_array($userQueryResult))
+    {
+        $userEmail = $row['reg_email'];
+        $userFirstName = $row['reg_fname'];
+        $userLastName = $row['reg_lname'];
+        $userDob = $row['reg_dob'];
+        $userPassword = $row['reg_password'];
+
+        $userXmlData .= 
+        "<user>".
+        "<email>".$userEmail ."</email>".
+        "<firstName>".$userFirstName."</firstName>".
+        "<lastName>".$userLastName."</lastName>".
+        "<dob>".$userDob."</dob>".
+        "<password>".$userPassword ."</password></user>";
+    }
+
+    $userXmlData .= "</users>";
+
+    $userXmlDataObject = simplexml_load_string($userXmlData) or die("Error: Cannot create object");
+    $userXmlDataObject ->asXML("xml/users.xml");
+
+    ?>
