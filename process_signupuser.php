8824eab928d226894bb3a6ade83589de1f4b0bf7
Nooree0413 <30431030+Nooree0413@users.noreply.github.com>
2017-11-15 23:00:28 +0400

    added xml code in the else condition

--- process_signupuser.php
+++ process_signupuser.php
@@ -16,7 +16,34 @@
 
 	else{
 		$query1 = mysql_query("INSERT INTO register VALUES ('$email','$firstname','$lastname','$dob','$password');") or die ("Query Failed" . mysql_error());
+		
+		$userQuery = "SELECT * FROM register";
+		$userQueryResult = mysql_query($userQuery) or die ('Query Failed' . mysql_error());
 	
+		$userXmlData = "<?xml version='1.0' encoding='UTF-8'?>
+		<users>";
+	
+		while ($row = mysql_fetch_array($userQueryResult))
+		{
+			$userEmail = $row['reg_email'];
+			$userFirstName = $row['reg_fname'];
+			$userLastName = $row['reg_lname'];
+			$userDob = $row['reg_dob'];
+			$userPassword = $row['reg_hashpassword'];
+	
+			$userXmlData .= 
+			"<user>".
+			"<email>".$userEmail ."</email>".
+			"<firstName>".$userFirstName."</firstName>".
+			"<lastName>".$userLastName."</lastName>".
+			"<dob>".$userDob."</dob>".
+			"<password>".$userPassword ."</password></user>";
+		}
+	
+		$userXmlData .= "</users>";
+	
+		$userXmlDataObject = simplexml_load_string($userXmlData) or die("Error: Cannot create object");
+		$userXmlDataObject ->asXML("users.xml");
 	}
 	header('Location:Welcomes.php');
 
