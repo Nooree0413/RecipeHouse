ec09ba204baa7f272b028c85241df629fdce58cf
Nirvan <nshewraj16@gmail.com>
2017-11-17 03:01:40 +0400

    added dom in "displayUserData.js" to retrieve email for user email from users.xml

--- js/displayXmlUserData.js
+++ js/displayXmlUserData.js
@@ -1,21 +1,19 @@
 if (window.XMLHttpRequest){
-        var xmlhttp = new XMLHttpRequest();
-    }
-
-else {
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    var xmlhttp = new XMLHttpRequest();
 }
 
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readystate == 4 && xmlhttp.status == 200)
-    {
-        retrieveUserData(xmlhttp);
-    }
+else {
+    var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
 
 xmlhttp.open("GET","xml/users.xml",false);
 xmlhttp.send();
+var xmlDoc = xmlhttp.responseXML;
+    
+xmlDoc = xmlDoc.getElementsByTagName("user");
+console.log(xmlDoc);
 
-function retrieveUserData (){
-    var xmlDoc = xmlhttp.responseXML;
+for (var i=0;i<xmlDoc.length;i++)
+{
+    document.getElementById("userEmail").innerHTML = xmlDoc[i].getElementsByTagName("email")[0].childNodes[0].nodeValue;
 }
