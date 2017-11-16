ed8fc4ccf0f8787e8a00f3a2828a9b5e4927631e
Nirvan <nshewraj16@gmail.com>
2017-11-17 03:27:36 +0400

    Loop through users.xml and display in admintask.php using DOM

--- js/displayXmlUserData.js
+++ js/displayXmlUserData.js
@@ -9,11 +9,21 @@ else {
 xmlhttp.open("GET","xml/users.xml",false);
 xmlhttp.send();
 var xmlDoc = xmlhttp.responseXML;
-    
+
 xmlDoc = xmlDoc.getElementsByTagName("user");
 console.log(xmlDoc);
 
+var userDetails = "<table><tr><th>Email Address</th><th>First Name</th><th>Last Name</th><th>Date Of Birth</th></tr>";
+
 for (var i=0;i<xmlDoc.length;i++)
 {
-    document.getElementById("userEmail").innerHTML = xmlDoc[i].getElementsByTagName("email")[0].childNodes[0].nodeValue;
+    userDetails += "<tr>";
+    userDetails += "<td>" + xmlDoc[i].getElementsByTagName("email")[0].childNodes[0].nodeValue + "</td>";
+    userDetails += "<td>" + xmlDoc[i].getElementsByTagName("firstName")[0].childNodes[0].nodeValue + "</td>";
+    userDetails += "<td>" + xmlDoc[i].getElementsByTagName("lastName")[0].childNodes[0].nodeValue + "</td>";
+    userDetails += "<td>" + xmlDoc[i].getElementsByTagName("dob")[0].childNodes[0].nodeValue + "</td>";
+    userDetails += "</tr>";
 }
+
+userDetails += "</table>"
+document.getElementById("adminTask_userTableContent").innerHTML = userDetails;
