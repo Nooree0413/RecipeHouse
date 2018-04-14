08d0ece9ed2750571e1c7ed995e06787b1118e16
Nooree0413 <30431030+Nooree0413@users.noreply.github.com>
2018-04-14 12:51:01 +0400

    added page for rest offline

---
+++ rest/client.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head></head>
+<body>
+	
+<form action='client.php' method='POST'>
+Temperature:
+<input type='text' name='temperature'>"
+<button type='submit' name='submit'>Fahenreit To Celcious</button>"
+</form>"
+
+
+</body></html>
+
+<?php
+	
+	if(isset($_POST['submit'])){
+
+		$tempFC=$_POST['temperature'];
+
+		$url="http://localhost/rest/$tempFC";
+
+		$client=curl_init($url);
+
+		curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+		
+		$response=curl_exec($client);
+
+		$result=json_decode($response);
+
+		echo $result->data;
+	}
+
+?>
