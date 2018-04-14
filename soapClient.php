23e7314fb278b71338184babb56ebb4f85746ef9
Nirvan <nshewraj16@gmail.com>
2018-04-14 21:22:59 +0400

    remove clutter from soap client.php

--- soapClient.php
+++ soapClient.php
@@ -1,25 +1,11 @@
-<!DOCTYPE <!DOCTYPE html>
-
-<html>
-    <head>
-
-    </head>
-    <body>
-        <h3>Enter values to calculate</h3>
-        <form name="frm1" action="soapClient.php" method="get">
-        Input Values:<input type="text" name="txt_ingredientName" value="fish"/>
-        <input name="btn_submit" type="submit"/>
-    </body>
-</html>
-
-
 <?php
     require_once "lib/nusoap.php";
-
+    
+    /*
     if (isset($_GET['txt_ingredientName'])){
         $ingredientName = $_GET['txt_ingredientName'];        
     }
-    
+    */
     $url = "http://www.supermarketapi.com/API.asmx?WSDL";
     $client = new nusoap_client($url,true);
 
@@ -30,19 +16,18 @@
     }
 
     $args = array("parameters" => array("APIKEY"=> "0fe9105274", 
-                    "ItemName"=> $ingredientName));
+                    "ItemName"=> "tomato"));
 
     $response = $client->call('SearchByProductName', $args); 
     
     $SearchByProductNameResultArray = $response["SearchByProductNameResult"];
     $productsArray = $SearchByProductNameResultArray["Product"];
     $itemArray = $productsArray[0];
-    echo $itemName = $itemArray["Itemname"];
-    echo $itemImageUrl = $itemArray["ItemImage"];
-    
+    $itemName = $itemArray["Itemname"];
+    $itemImageUrl = $itemArray["ItemImage"];
+    $itemCategory = $itemArray["ItemCategory"];
 
-
-    echo "<br/>Response: " . print_r($response);
+    // echo "<br/>Response: " . print_r($response);
 /*    
     echo '<h2>Request</h2>';
     echo '<pre>' . htmlspecialchars($client->request, ENT_QUOTES) . '</pre>';
